d162d6cd0469650cd063914ef8625084b3bd19f3
Bagas <bagashyt@gmail.com>
2022-07-15 12:06:31 +0700

    clean code

--- app/src/main/java/com/bagashyt/myintermediate/MainActivity.kt
+++ app/src/main/java/com/bagashyt/myintermediate/MainActivity.kt
@@ -13,48 +13,10 @@ import androidx.core.content.getSystemService
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var myButton: MyButton
-    private lateinit var myEditText: MyEditText
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        myButton = findViewById(R.id.my_button)
-        myEditText = findViewById(R.id.my_edit_text)
 
-        setMyButtonEnable()
-
-        myEditText.addTextChangedListener(object : TextWatcher{
-            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-
-            }
-
-            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
-                setMyButtonEnable()
-            }
-
-            override fun afterTextChanged(p0: Editable?) {
-            }
-
-        })
-
-        myButton.setOnClickListener{
-            myEditText.hideSoftInput()
-            Toast.makeText(this@MainActivity, myEditText.text, Toast.LENGTH_SHORT).show()
-        }
-
-    }
-
-    private fun setMyButtonEnable(){
-        val result = myEditText.text
-        val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
-        myButton.isEnabled = result != null && result.toString().isNotEmpty()
-
-    }
-
-     fun View.hideSoftInput(){
-        val inputMethodManager = getSystemService(Activity.INPUT_METHOD_SERVICE) as InputMethodManager
-        inputMethodManager.hideSoftInputFromWindow(windowToken, 0)
     }
 }
