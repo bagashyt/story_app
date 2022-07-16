f4be4b38e5c0d2a1db70412428c8de33c12e3f37
Bagas <bagashyt@gmail.com>
2022-07-17 04:01:59 +0700

    add passwordEditText

--- app/src/main/java/com/bagashyt/myintermediate/MainActivity.kt
+++ app/src/main/java/com/bagashyt/myintermediate/MainActivity.kt
@@ -1,15 +1,7 @@
 package com.bagashyt.myintermediate
 
-import android.app.Activity
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.view.View
-import android.view.inputmethod.InputMethod
-import android.view.inputmethod.InputMethodManager
-import android.widget.Toast
-import androidx.core.content.getSystemService
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
